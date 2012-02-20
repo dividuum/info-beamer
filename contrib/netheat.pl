67e7b8a16df33c5233c899150f77dffd08e7e649
Florian Wesch <fw@dividuum.de>
2012-02-20 22:31:37 +0100

    updated

--- contrib/netheat.pl
+++ contrib/netheat.pl
@@ -3,18 +3,19 @@
 use 5.010;
 use strict;
 use warnings;
+use bignum;
 use Carp;
 use SNMP::Info;
 use Data::Dumper;
 use Net::OpenSoundControl::Client;
 use Time::HiRes qw(sleep);
-
-use bignum;
+use Math::BigFloat;
+use List::Util qw(min);
 
 my $sleep_time = 1;
 my $snmp = SNMP::Info->new(AutoSpecify => 0,
                            Debug => 0,
-                           DestHost => '192.168.24.1',
+                           DestHost => '10.0.0.254',
                            Community => 'public',
                            Version => 2,
                            BigInt => 1)
@@ -25,49 +26,58 @@ my $osc_client = Net::OpenSoundControl::Client->new(
                                                     Port => 4444)
   or carp "could not initialize osc";
 
-my $interfaces = $snmp->interfaces();
+my @interface_whitelist = ('alicedsl','qscdsl','eth2');
+
+my @interfaces = grep {
+  $_->{name} ~~ @interface_whitelist and
+    not exists $snmp->if_ignore()->{$_->{idx}} and
+      $snmp->i_up()->{$_->{idx}} eq 'up' and
+        $snmp->i_up_admin()->{$_->{idx}} eq 'up';
+} map {
+    {idx=>$_, name=>$snmp->i_name()->{$_}}
+} keys $snmp->interfaces();
 
 my %override_max_speed = (
-                          'wan' => 17_000_000
+                          'wan' => 17_000_000,
+                          'alicedsl' => 17_000_000
                          );
 
-my @interface_whitelist = ( 'entropia-wlan', 'entropia-eth', 'entropia-clubbus',
-                            'henet', 'qdsl', 'entropia-voip' );
+my %speeds = map {
+    (
+     $_->{name} =>
+     ($override_max_speed{$_->{name}} // $snmp->i_speed_raw()->{$_->{idx}}
+      or croak("no speed for interface ".$_->{name}))/8
+    )
+} @interfaces;
 
 sub derive_val  {
   state %cache;
   my ($name, $idx) = @_;
   my $counter = $snmp->i_octet_in64()->{$idx} + $snmp->i_octet_out64()->{$idx};
-  my $speed = $override_max_speed{$name} // $snmp->i_speed_raw()->{$idx} or carp "no interface speed available";
   my $r = ($counter - ($cache{$idx} // $counter)) / $sleep_time;
   $cache{$idx} = $counter;
-  return ($r <= 0) ? 0 : log($r)/log($speed);
+  return min(1,$r/$speeds{$name});
 }
 
 sub send_val {
   my $val = shift;
   $osc_client->send($val);
-  say $val->[0] . ": " . $val->[2];
+  say join ', ', @$val;
 }
-  
-  while (1) {
-    eval {
-      $snmp->load_i_octet_in64();
-      $snmp->load_i_octet_out64();
-      map {
-        send_val(['/gauge/if/' . $_->{name}, 'f', derive_val($_->{name}, $_->{idx})]);
-      } grep {
-        $_->{name} ~~ @interface_whitelist and
-          not exists $snmp->if_ignore()->{$_->{idx}} and
-            $snmp->i_up()->{$_->{idx}} eq 'up' and
-              $snmp->i_up_admin()->{$_->{idx}} eq 'up';
-      } map {
-        {idx => $_, name => $snmp->i_name()->{$_}}
-      } sort keys $interfaces;
-      1;
-    } or do {
-      print STDERR "error sending counter values: " . $@;
-    };
-    sleep($sleep_time);
-  }
+
+while (1) {
+  eval {
+    $snmp->load_i_octet_in64();
+    $snmp->load_i_octet_out64();
+    map {
+      send_val(['/gauge/if/' . $_->{name},
+                'f',
+                derive_val($_->{name}, $_->{idx})]);
+    } @interfaces;
+    1;
+  } or do {
+    print STDERR "error sending counter values: " . $@;
+  };
+  sleep($sleep_time);
+}
 
